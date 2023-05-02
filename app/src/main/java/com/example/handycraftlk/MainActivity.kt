59a38381a7e6d9838ff995a4655c6c3715836090
Bavantha-Senanayake <bavanthasenanayake@gmail.com>
2023-05-02 15:46:20 +0530

    mmmm commit

--- app/src/main/java/com/example/handycraftlk/MainActivity.kt
+++ app/src/main/java/com/example/handycraftlk/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.handycraftlk
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.handycraftlk.databinding.ActivityMainBinding
+import com.google.firebase.database.DatabaseReference
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+
+
+
+
     }
 }
