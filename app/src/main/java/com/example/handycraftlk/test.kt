a708fca4d12d9cead98f73409e2c1c5ba49b89fa
milni-premathilake <milnipremathilaka@gmail.com>
2023-05-02 16:53:47 +0530

    milni commit mmm

--- app/src/main/java/com/example/handycraftlk/test.kt
+++ app/src/main/java/com/example/handycraftlk/test.kt
@@ -3,10 +3,9 @@ package com.example.handycraftlk
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class test : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-       getSupportActionBar()?.hide()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_test)
     }
 }
