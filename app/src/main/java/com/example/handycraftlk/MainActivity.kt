300aa5da9faff443beb2e45a36514f60d9a1e26a
JanithGunathilake <janithkavinda49@gmail.com>
2023-05-02 18:34:53 +0530

    login and register done

--- app/src/main/java/com/example/handycraftlk/MainActivity.kt
+++ app/src/main/java/com/example/handycraftlk/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.handycraftlk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.handycraftlk.databinding.ActivityAccInfomationBinding
 import com.example.handycraftlk.databinding.ActivityMainBinding
 import com.google.firebase.database.DatabaseReference
 
 class MainActivity : AppCompatActivity() {
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
 
 
 
 
     }
+
+    fun buttonLogin(v: View){
+
+        val intent = Intent(this, MainActivity_register::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
 }
