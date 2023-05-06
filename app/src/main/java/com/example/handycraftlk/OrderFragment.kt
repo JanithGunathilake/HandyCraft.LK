722a986ec483c1d260effa5da18003d3e6f1d041
JanithGunathilake <janithkavinda49@gmail.com>
2023-05-06 20:30:54 +0530

    commit for bawwa

--- app/src/main/java/com/example/handycraftlk/OrderFragment.kt
+++ app/src/main/java/com/example/handycraftlk/OrderFragment.kt
@@ -1,20 +1,35 @@
 package com.example.handycraftlk
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.handycraftlk.R
 
 class OrderFragment : Fragment() {
+
+    private lateinit var btnClickHere : Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_order, container, false)
+        btnClickHere = view.findViewById(R.id.btnClickHere)
+        btnClickHere.setOnClickListener {
+
+            val intent = Intent(activity, COrdersFragmentMain::class.java)
+            startActivity(intent)
+            activity?.finish()
+
+        }
+
+        return view
     }
+
 
 }
