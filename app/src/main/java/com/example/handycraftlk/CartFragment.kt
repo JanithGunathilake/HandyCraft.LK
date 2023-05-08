af7457d1f53c73bb9463d5ad3458b4f3f3204d5d
Bavantha-Senanayake <bavanthasenanayake@gmail.com>
2023-05-08 16:18:07 +0530

    done and dusted

--- app/src/main/java/com/example/handycraftlk/CartFragment.kt
+++ app/src/main/java/com/example/handycraftlk/CartFragment.kt
@@ -32,7 +32,10 @@ class CartFragment : Fragment() {
         val checkoutButton = view.findViewById<Button>(R.id.checkoutButton)
         if (checkoutButton != null) {
             checkoutButton.setOnClickListener {
-                removeCartDataForUser()
+                // Add an Intent to start the CustomerCheckout activity
+                val intent = Intent(activity, CustomerCheckout::class.java)
+                startActivity(intent)
+                //removeCartDataForUser()
             }
         }
 
@@ -81,31 +84,7 @@ class CartFragment : Fragment() {
             })
     }
 
-    private fun removeCartDataForUser() {
-        val userEmail = sessionManager.getEmail()
-        dbref = FirebaseDatabase.getInstance().getReference("Cart")
 
-        dbref.orderByChild("buyerEmail").equalTo(userEmail)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(snapshot: DataSnapshot) {
-                    if (snapshot.exists()) {
-                        for (cartSnapshot in snapshot.children) {
-                            val cart = cartSnapshot.getValue(Cart::class.java)
-                            if (cart?.buyerEmail == userEmail) {
-                                cartSnapshot.ref.removeValue()
-                            }
-                        }
-                        // Add an Intent to start the CustomerCheckout activity
-                        val intent = Intent(activity, CustomerCheckout::class.java)
-                        startActivity(intent)
-                    }
-                }
-
-                override fun onCancelled(error: DatabaseError) {
-                    // Handle error here
-                }
-            })
-    }
 
 
 
